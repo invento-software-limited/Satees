86e3f0a10e839be4a9e3b0e3e375154b6e94e2ac
Nazmul Hossain <nazmul@invento.com.bd>
2026-03-19 15:54:17 +0600

    add: PDF to Purchase Receipt

---
+++ satees/satees/doctype/pdf_to_purchase_receipt/pdf_to_purchase_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Abdul Hasib and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PDF to Purchase Receipt", {
+// 	refresh(frm) {
+
+// 	},
+// });
